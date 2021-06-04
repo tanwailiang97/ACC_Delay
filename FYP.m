9a869e5f403fcf592fb3f2d97b13a6db08bd78cf
unknown <tanwailiang97@gmail.com>
2021-06-04 21:15:21 +0800

    Solved double problem

--- FYP.m
+++ FYP.m
@@ -15,13 +15,13 @@ fprintf("%s\nStarting FYP ...\n",datetime('now','Format','y-MMM-d HH-mm-ss'));
 
 maxParallel = 100;    
 
-parfor parallel = 1:maxParallel
+parfor parallel = 8:maxParallel
     sensD = ceil(parallel/10)*0.1;
     minReward = globalVar(5);
     accFactor = globalVar(6);
     disFactor = globalVar(7);
-    fprintf("%s\nStarting Parallel...%d\n",datetime('now','Format','y-MMM-d HH-mm-ss'),parallel);
-    maxWorker = 14;
+    fprintf("%s\nStarting Parallel...%d,%f\n",datetime('now','Format','y-MMM-d HH-mm-ss'),parallel,sensD);
+    maxWorker = 10;
     param = [0.000 -2.1 4.42 1.3 0.81 0.55];
     prevResult = minReward;
     step = 0.01*2^9;
@@ -34,10 +34,10 @@ parfor parallel = 1:maxParallel
     while(1)
         for worker = 3:maxWorker
             %fprintf("Running Worker %d\n",worker);
-            if worker > 13 || prevResult == minReward
+            if worker > 9 || prevResult == minReward
                 newParam(worker,:) = [0,(randi(801,1,1)-801)*0.01,(randi(801,1,2)-1)*0.01,0,0];
                 %newParam(worker,:) = [0,(randi(1600,1,5)-800)*0.01];
-            elseif worker == 13
+            elseif worker == 9
                 newParam(worker,:) = param;
             else
                 identity = eye(6);
--- globalVar.m
+++ globalVar.m
@@ -1,11 +1,11 @@
 function x = globalVar(num)
     TOTAL_TIME = 170;    %total Time for simulation
     PERIOD = 0.01;      %10ms global sampling 
-    SENS_DELAY_1 = 1.0; %200ms sensor delay for Leader
+    SENS_DELAY_1 = 0.1; %200ms sensor delay for Leader
     SENS_DELAY_2 = SENS_DELAY_1; %200ms sensor delay for Preceder
     SENS_PERIOD = SENS_DELAY_1; %60ms update rate
     PREV_RESULT = -18*10^80;
-    ACC_FACTOR = 5;%randi(20)+5;
+    ACC_FACTOR = 10;%randi(20)+5;
     DIS_FACTOR = 10;%randi(4)+1;
     DELAY_COMPENSATED  = 0;
     EXTRA_DIS = 2;
